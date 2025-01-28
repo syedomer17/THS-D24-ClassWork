8fda2c81e1d7c3bf02a2f74b00ab6af85a73128d
syedomer17 <syedomerali2006@gmail.com>
2025-01-28 09:22:19 +0530

    advance

---
+++ asyn-javaScript/coustom-promises/2.js
@@ -0,0 +1,25 @@
+function Age(a){
+    const p = new Promise((resolve, reject) => {
+        if(a>=18){
+            resolve("allowed")
+        }else{
+            reject("Not allowed")
+        }
+    })
+    return p;
+}
+async function main() {
+    try {
+        let data = await Age(19);
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+Age(18)
+.then((data)=>{
+    console.log(data);
+})
+.catch((error)=>{
+    console.log(error);
+})
