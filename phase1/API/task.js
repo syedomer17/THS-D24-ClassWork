39cf2e71697699d9ab47cb4bfbcd70ebff0ef63e
syedomer17 <syedomerali2006@gmail.com>
2025-01-31 21:49:10 +0530

    phase-1

---
+++ phase1/API/task.js
@@ -0,0 +1,15 @@
+/*
+https://appserver-w05c.onrender.com/add/10/20
+
+create a program where you ask the user to to enter n1 and n2 
+1.make a request to get the sum1 = n1+n2 and then ask the user to enter n3 
+2.make  a request to get the sum2 = n1+sum1 and then as the user for enter n4
+3.make a  request to get the sum3=sum2+n4 and print it 
+
+use nested .then .catch 
+
+readline-sync
+
+axios
+
+*/
