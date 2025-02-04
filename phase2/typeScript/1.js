d469cd30166c7570abe1fd803edfcdbf227f3360
syedomer17 <syedomerali2006@gmail.com>
2025-02-04 06:49:44 +0530

    typeScript-intro

---
+++ phase2/typeScript/1.js
@@ -0,0 +1,15 @@
+function sum(a, b) {
+  return a + b;
+}
+console.log(sum(1, 2));
+var user = {
+  age: 18,
+  bootCamp: "THS-D24",
+};
+function getAge(user) {
+  console.log(user.age);
+}
+getAge(user);
+var array = ["1", "2", "3"];
+array.push("5");
+console.log(array);
---
+++ phase2/typeScript/2.ts
@@ -0,0 +1,43 @@
+/*
+Interfaces
+type
+*/
+
+// let user = {
+//   age: 21,
+//   bootcamp: "THS D24",
+// };
+
+// function getAge(user: { age: number, bootcamp: string }): number {
+//     return user.age
+// }
+
+// getAge(user)
+
+//----------------------------------------------
+
+// interface User {
+//     age: number;
+//     bootcamp: string;
+// }
+
+type User = {
+  age: number;
+  bootcamp: string;
+};
+
+function getAge(user: User): number {
+  return user.age;
+}
+
+function getBootcamp(user: User): string {
+  return user.bootcamp;
+}
+
+// AVOID
+// function getUser(user) {
+//     return user;
+// }
+// getUser("hello")
+
+//----------------------------------------------
